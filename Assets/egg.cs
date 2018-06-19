364f8a5accab1872cd93a21f7c5cbe77a2cf957d
Jenny Robertson <jenrobertsonjen@gmail.com>
2018-06-19 18:05:17 +0100

    sort out gitignore

---
+++ Assets/egg.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class egg : MonoBehaviour {
+
+	public int weight;
+	public bool empty;
+
+	// Use this for initialization
+	void Start () {
+		int randomNumber = Random.Range(0, 2);
+		if (randomNumber == 0) {
+			empty = true;
+			weight = Random.Range(3, 6);
+		}
+		else {
+			empty = false;
+			weight = Random.Range(10, 16);
+		}
+
+		Debug.Log(weight);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/weigher.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class weigher : MonoBehaviour {
+	//todo
+	//add dial animation at correct time
+	//add pot animation at right time
+	//add removing egg pot animation at right time + make it
+	//make it animate back to zero properly?
+
+	//make eggs bigger based on their weight? hmm
+	//add proper collider to the weigher
+
+	public Transform dial;
+	public Transform pot;
+
+	private float zAngleTarget;
+	private float secondsToLerp;
+	float t;
+
+	void Start () {
+		t = 0;
+		zAngleTarget = 0;
+		secondsToLerp = 1;
+	}
+	
+	void Update () {
+		t += Time.deltaTime / secondsToLerp;
+		dial.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(0,zAngleTarget, t));
+	}
+
+	void OnTriggerEnter (Collider collider) {
+		if (collider.tag == "egg") {
+			rotateDial(collider.gameObject.GetComponent<egg>().weight);
+		}
+		Debug.Log("go!");
+	}
+ 
+ 	void OnTriggerExit (Collider collider) {
+		if (collider.tag == "egg") {
+			rotateDial(0);
+		}
+	}
+
+	void rotateDial(int degrees){
+		t = 0;
+		zAngleTarget = -degrees * 19;
+	}
+}
